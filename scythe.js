d36494138776f785057b4ce381e39d58f8cbd111
Richard0820 <77077277+Whyisthisnotavalable@users.noreply.github.com>
2023-08-03 19:47:19 -0700

    0.16

--- scythe.js
+++ scythe.js
@@ -35,7 +35,7 @@ javascript:(function() {
             name: "drawn out",
             link: `<a target="_blank" href='https://en.wikipedia.org/wiki/Forging' class="link">drawn out</a>`,
             descriptionFunction() {
-                return `Increases <strong>scythe</strong> blade length by 1<br>increase scythe <strong class="color-d">damage</strong> by <strong>30%</strong>`
+                return `Increases <strong>scythe</strong> blade length by <strong>1</strong><br>increase scythe <strong class="color-d">damage</strong> by <strong>30%</strong>`
             },
             isGunTech: true,
             maxCount: 1,
@@ -57,7 +57,7 @@ javascript:(function() {
             name: "Ti-6Al-4V",
             link: `<a target="_blank" href='https://en.wikipedia.org/wiki/Ti-6Al-4V' class="link">Ti-6Al-4V</a>`,
             descriptionFunction() {
-                return `Increase <strong>scythe</strong> duration by 10%<br>increase scythe <strong class="color-d">damage</strong> by <strong>15%</strong>`
+                return `Increase <strong>scythe</strong> duration by <strong>10%</strong><br>increase scythe <strong class="color-d">damage</strong> by <strong>15%</strong>`
             },
             isGunTech: true,
             maxCount: 3,
